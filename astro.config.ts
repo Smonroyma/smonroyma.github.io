657d6ad2ebbc7e7a82b5d26fe722dd8086433771
Smonroyma <smonroyma@unbosque.edu.co>
2026-02-15 18:21:39 -0500

    Configure Astro with site URL and integrations
    Added site URL and integrated various plugins for enhanced functionality.

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,34 @@
-import { defineConfig } from 'astro';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+import expressiveCode from 'astro-expressive-code';
+import spectre from './package/src';
+import { spectreDark } from './src/ec-theme';
 
 export default defineConfig({
-  output: 'static',
-  // other configurations
+	site: 'https://smonroyma.github.io',
+	output: 'static', // GitHub Pages necesita esto
+	integrations: [
+		expressiveCode({
+			themes: [spectreDark],
+		}),
+		mdx(),
+		sitemap(),
+		spectre({
+			name: 'Santiago Monroy',
+			openGraph: {
+				home: {
+					title: 'Presentación - Santiago Monroy',
+					description: 'Portafolio para la asignatura de Bases de Datos 1.',
+				},
+				blog: {
+					title: 'Evidencias',
+					description: 'Seguimiento del curso de Bases de Datos.',
+				},
+				projects: {
+					title: 'Proyecto Final',
+				},
+			},
+		}),
+	],
 });

ee4ba69f5b1d102c940fc398a0f5c987965732ff
Smonroyma <smonroyma@unbosque.edu.co>
2026-02-15 15:48:53 -0500

    Update site URL in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -21,7 +21,7 @@ const {
 
 // https://astro.build/config
 const config = defineConfig({
-	site: 'https://spectre.lou.gg',
+	site: 'https://smonroyma.github.io',
 	output: 'static',
 	integrations: [
 		expressiveCode({
